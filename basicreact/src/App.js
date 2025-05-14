23a30d44fd9e850368822f465819f92e097ea093
Sandesh Khadka <khadkasandesh696@gmail.com>
2025-05-15 00:17:51 +0545

    third commit using vite

--- basicreact/src/App.js
+++ basicreact/src/App.js
@@ -1,8 +1,9 @@
 
 
 function App() {
+  const username='sandesh';
   return (
-    <h1>hello sandesh</h1>
+    <h1>hello {username}</h1>//{javascript variable} we can pass like this(this is evaluated js )
   )
 }
 
