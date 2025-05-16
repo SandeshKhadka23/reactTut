a9b048fa5115ee2b17ddd4bfe9a99e312f60dc95
Sandesh Khadka <khadkasandesh696@gmail.com>
2025-05-17 00:02:10 +0545

    useState hook

--- ViteProject/src/App.jsx
+++ ViteProject/src/App.jsx
@@ -1,23 +1,12 @@
-
-import Greet from "./components/greet";
-import Header from "./components/header";
-import{Footer} from "./components/footer";
-
-
+import React from 'react';
+import Counter from './Counter';
 
 function App() {
-  
-
   return (
     <div>
-    <Header header="Welcome to our website"/> 
-    <Greet name="sandesh"/>
-    <Greet name="RCB"/>
-    <Footer footer="Thanks for visiting our website"/>
-     {/* </> fomat */}
-   <h1>Hello!!</h1>
-</div>
-  )
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
